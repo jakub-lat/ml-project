bf96d93d09ec70cd8a82c7882228c5441a8b04fb
Jakub <jakub@cubepotato.eu>
2021-06-18 18:37:00 +0200

    train.py

--- train.py
+++ train.py
@@ -5,21 +5,21 @@ import trainer as trainer
 from model import model, loss
 import json
 
-def train(config: dict):
+
+def train(experiment, data_path, batch_size, n_epochs):
     torch.cuda.empty_cache()
-    (train_set, test_set), (train_loader, test_loader) = data.load_data(config['data_path'], 16)
+    (train_set, test_set), (train_loader, test_loader) = data.load_data(data_path, batch_size)
     criterion = loss.get_loss()
     net = model.Net(len(train_set.classes)).type(torch.float32)
 
-    experiment = Experiment(
-        api_key=config['comet_api_key'],
-        project_name=config['comet_project_name'],
-        workspace=config['comet_workspace'],
-    )
-
-    trainer.train(net, loader=train_loader, criterion=criterion, device='cuda', experiment=experiment, n_epochs=10)
+    trainer.train(net, loader=train_loader, criterion=criterion, device='cuda', experiment=experiment, n_epochs=n_epochs)
 
 
 if __name__ == '__main__':
     config = json.load('config.json')
-    train(config)
+    e = Experiment(
+        api_key=config['comet_api_key'],
+        project_name=config['comet_project_name'],
+        workspace=config['comet_workspace'],
+    )
+    train(e, *config)
